aeeb3e72d6c42114d9c65e65eaec5158dbb28cb7
jandedeurwaerder-howest <jan.de.deurwaerder@howest.be>
2023-05-25 11:51:52 +0200

    Add project files.

---
+++ Scala.ADOAutos.Core/Entities/Voertuig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scala.ADOAutos.Core.Entities
+{
+    public class Voertuig
+    {
+        public string Merk { get; set; }
+        public string Serie { get; set; }
+        public string Kleur { get; set; }
+        public string Nummerplaat { get; set; }
+        public int KW { get; set; }
+        public DateTime InDienst { get; set; }
+
+        public Voertuig()
+        { }
+        public Voertuig(string merk, string serie, string kleur, string nummerplaat, int kw, DateTime inDienst)
+        {
+            Merk = merk;
+            Serie = serie;
+            Kleur = kleur;
+            Nummerplaat = nummerplaat;
+            KW = kw;
+            InDienst = inDienst;
+        }
+        public override string ToString()
+        {
+            return $"{Merk} - {Serie}";
+        }
+    }
+}
